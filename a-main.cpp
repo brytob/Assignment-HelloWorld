8057dfeddd099971967be69d03b18025c1e22d93
brytob <bryan.tobias@lethpolytech.ca>
2024-09-07 23:32:46 -0600

    Reviewed Helloe Woerld

---
+++ a-main.cpp
@@ -0,0 +1,32 @@
+/**
+@file       a-main.cpp
+@author     Bryan Tobias (s0460191) (signed - bt)
+@date       08/09/2024
+@brief      This is Assignment 0 - Hello World
+**/
+
+// put the #include directive here for the io stream(bt:done)
+#include <iostream>
+
+// put the using directive here to use the entire std namespace (bt:done)
+using namespace std;
+
+int main() {
+	/** 'cout <<' with '\n' were added to all three statements to build a paragraph
+	alternatively it'd be written in a single line but this is more legible**/
+
+	cout << "Hello, World!\n";
+	cout << "My name is Bryan Tobias.\n";
+	cout << "Pleased to meet you.\n";
+
+	// remember to replace <placeholder> with your name (bt:done.)
+	
+	/**also, each line need an endl character
+	
+	bt: I attempted to type endl but I couldn't 
+	find a way to do so without any of the three 
+	compilers I've used giving me an error.
+	**/
+
+	return 0;
+}
